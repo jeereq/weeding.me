8b73969a67c20d5a8ff607c423a704042470df09
minganda <mingandajeereq@gmail.com>
2025-04-25 12:58:35 +0100

    feat: save change

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import localFont from "next/font/local";
 
 export const metadata: Metadata = {
-  title: 'InviteVite | Invitations Électroniques Élégantes et Personnalisables',
+  title: 'Weeding.me | Invitations Électroniques Élégantes et Personnalisables',
   description: 'Créez des invitations électroniques élégantes et personnalisables pour vos mariages, anniversaires et événements spéciaux. Simple, écologique et mémorable.',
 };
 
