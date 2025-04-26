ddfe7a7d56eddb72af48576b2a075f21cdce5cf2
minganda <mingandajeereq@gmail.com>
2025-04-26 18:51:47 +0100

    feat: save change

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -86,7 +86,7 @@ export default function Templates() {
                         alt={template.title}
                         className="object-cover h-full w-full transition-transform duration-500 group-hover:scale-110"
                       />
-                      <div className="absolute inset-0 gap-2 bg-gradient-to-t from-background/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-6">
+                      <div className="absolute inset-0 grid lg:grid-cols-2 gap-2 bg-gradient-to-t from-background/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-end p-6">
                         <Button
                           variant="secondary"
                           className="w-full backdrop-blur-sm"
