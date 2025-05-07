4e89b0a17ee677bd5bae6859096c40ddb098bbaf
minganda <mingandajeereq@gmail.com>
2025-05-07 16:47:54 +0100

    feat: save change

--- components/ui/templateImages.tsx
+++ components/ui/templateImages.tsx
@@ -5,9 +5,9 @@ export default function TemplateImages({ setImage = function () { } }: any) {
     const [images] = useState([
         { id: 1, uri: "https://i.pinimg.com/736x/a5/b6/14/a5b614c1975ed48ad4f4927194cc0d03.jpg" },
         { id: 2, uri: "https://i.pinimg.com/736x/e0/08/11/e0081100d4f1a60459716c3ebe0b40e2.jpg" },
-        { id: 3, uri: "https://i.pinimg.com/736x/e0/08/11/e0081100d4f1a60459716c3ebe0b40e2.jpg" },
+        { id: 3, uri: "https://i.pinimg.com/736x/90/01/c4/9001c49e3f07b58079b7e996232b7397.jpg" },
         { id: 4, uri: "https://i.pinimg.com/736x/41/1a/75/411a75e6ce26301e1381c1bb51e35aa2.jpg" },
-        { id: 5, uri: "https://i.pinimg.com/736x/e0/08/11/e0081100d4f1a60459716c3ebe0b40e2.jpg" }
+        { id: 5, uri: "https://i.pinimg.com/736x/f8/fb/c7/f8fbc71d5ddc970e4f1a34bb40c337c8.jpg" }
     ])
     return <div className="lg:fixed z-40 left-0 top-0 bottom-0 w-fit px-2 h-full flex lg:flex-col items-center justify-center">
         {images.map(function ({ uri }) {
